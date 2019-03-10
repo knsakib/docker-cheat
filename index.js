f3f9e56f9b3aec2c854d202bbcff3e38e313e651
khalidsakib <knsakib@gmail.com>
2019-03-10 18:48:55 -0400

    Docker Compose

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const redis = require('redis');
 
 const app = express();
+const client = redis.createClient({
+  host: 'redis-server', //when our express srever try to connect to the redis-server, docker will look for the container called-redis-server.
+  //This is how node app will connect to redi, as in our docker-compose we mentioned which container is redis-server
+  port: 6379
+});
+client.set('visits', 0);
 
 app.get('/', (req, res) => {
-  res.send('How are you doing');
+  client.get('visits', (err, visits) => {
+    res.send('Number of visits is ' + visits);
+    client.set('visits', parseInt(visits) + 1);
+  });
 });
 
 app.listen(8080, () => {
